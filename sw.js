d5782ac5accb9b23c0c69b0bb6312bfead076be4
Akasia Moon <akasiamoonshadow@gmail.com>
2026-06-06 16:05:04 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = 'somniascapes-v1';
+const ASSETS = [
+    './index.html',
+    './manifest.json',
+    './sw.js',
+    // Add paths to your images and audio files here to cache them for offline use
+];
 
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
